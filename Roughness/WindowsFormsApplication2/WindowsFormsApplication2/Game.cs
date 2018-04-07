1278a12b574ef9a5291b2200c15ba2ad7bd01bb9
TheSacrament <andronss75@yandex.ru>
2018-04-07 16:45:02 +0500

    v 0.0.1
    - Создан проект и заложена основная структура.

---
+++ Roughness/WindowsFormsApplication2/WindowsFormsApplication2/Game.cs
@@ -0,0 +1,250 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WindowsFormsApplication2 {
+    public class GameMap {
+        public bool[][] CollisionsMap; //Карта коллизий
+        public GameMap(int x, int y) {
+            CollisionsMap = new bool[x + 50][];
+            for (int i = 0; i < x + 50; i++) CollisionsMap[i] = new bool[y + 50];
+        }
+
+    }
+    static class Game {
+        static public void star(Form1 cf) {
+            Form1 curren_form1 = cf;
+            curren_form1.game_map = new GameMap((int)cf.ClientSize.Width, (int)cf.ClientSize.Height);
+            List<Bomb> bombs = new List<Bomb>();
+
+            Player one = new Player(curren_form1, "P1", 1, 1, 50, 50, @"..\..\GameRes\Image1.png"); // TODO Перед сборпкой обязательно указать новые пути
+            one.setKeysControl(Keys.Left, Keys.Up, Keys.Right, Keys.Down, Keys.Space);
+            one.putBomb += ((int x, int y) => { bombs.Add(new Bomb(curren_form1, x, y)); });
+
+            Player two = new Player(curren_form1, "P2", 400, 400, 50, 50, @"..\..\GameRes\Image1.png"); // TODO Перед сборпкой обязательно указать новые пути
+            two.setKeysControl(Keys.A, Keys.W, Keys.D, Keys.S, Keys.F);
+            two.putBomb += ((int x, int y) => { bombs.Add(new Bomb(curren_form1, x, y)); });
+
+            List<Wall> walls = new List<Wall>();
+            for (int x = 50; x <= 600; x += 100) for (int y = 50; y <= 500; y += 100) walls.Add(new Wall(curren_form1, "iw", x, y, 48, 48));
+
+            cf.game_timer.Interval = 10; // Настройка таймера
+            cf.game_timer.Start();
+        }
+    }
+
+    abstract class GameObject {
+        protected const int COLLISIONS_COEFFICIENT = 3;
+        static readonly int k_max_x;
+        static readonly int k_max_y;
+        public PictureBox body; // Тело отрисовки
+        private int m_curr_position_x; // Текущая позиция на экране в пикселях, при отображении реально находиться в левом верхнем углу
+        private int m_curr_position_y;
+        protected readonly int m_size_x; // Размер, используеться и при отрисовке и при взаимодействии
+        protected readonly int m_size_y;
+        protected int m_center_x; // Центр, каждый раз при изменений х и у пересчитываеться
+        protected int m_center_y;
+        protected Direction curren_direction;
+        private string m_id;
+        protected Form1 curren_form;
+        public Point m_location;
+
+
+        //protected GameObject() : this(null, "", 0, 0, 0, 0) { }
+        protected GameObject(Form1 cf, string id, int x, int y, int x_size, int y_size) : this(cf, id, x, y, x_size, y_size, null) { }
+        public GameObject(Form1 cf, string id, int x, int y, int x_size, int y_size, string image_path) {
+            curren_form = cf;
+            m_location = new System.Drawing.Point(x, y);
+            body = new PictureBox();
+            this.x = x;
+            this.y = y;
+            m_size_x = x_size;
+            m_size_y = y_size;
+            m_center_x = x + (m_size_x / 2);
+            m_center_y = y + (m_size_y / 2);
+            body.Size = new System.Drawing.Size(x_size, y_size);
+            body.ClientSize = body.Size;
+            body.Location = m_location;
+            body.Name = id;
+            body.Text = "";
+            body.TabIndex = 0;
+            m_id = id;
+            if (image_path != null) body.Image = Image.FromFile(image_path);
+            else body.BackColor = Color.Bisque;
+            body.SizeMode = PictureBoxSizeMode.StretchImage; // Растягивание текстуры
+            cf.Controls.Add(body);
+        }
+        static GameObject() {
+            k_max_x = 0;// TODO потом исправить
+            k_max_y = 0;
+        }
+
+        public int x {
+            get {
+                return m_curr_position_x;
+            }
+            set {// Если < 0 то будем выходить за пределы массивов при использовании x
+                if ((value < 1) || (value > curren_form.ClientSize.Width - m_size_x)) return;
+
+                m_location.X = value;
+                body.Location = m_location;
+                m_center_x = x + (m_size_x / 2);
+                m_curr_position_x = value;
+            }
+        }
+        public int y {
+            get {
+                return m_curr_position_y;
+            }
+            set {// Если < 0 то будем выходить за пределы массивов при использовании y
+                if ((value < 1) || (value > curren_form.ClientSize.Height - m_size_y)) return;
+                m_location.Y = value;
+                body.Location = m_location;
+                m_center_y = y + (m_size_y / 2);
+                m_curr_position_y = value;
+            }
+        }
+    }
+
+    class Player : GameObject, IMortal, IAbleToMove {
+        private int move_speed { get; set; } // Скорость передвижения игрока
+        private List<Keys> keys_control = new List<Keys>(); // Клавиши управления
+        private Dictionary<Keys, bool> keys_now_presed = new Dictionary<Keys, bool>(); // Флаги нажатых в данный момент клавиш управления
+        public event Action<int, int> putBomb;
+
+        public Player(Form1 cf, string id, int x, int y, int x_size, int y_size, string image_path) : base(cf, id, x, y, x_size, y_size, image_path) {
+            curren_form.KeyDown += new KeyEventHandler(ActionByKeyDown);
+            curren_form.KeyUp += new KeyEventHandler(ActionByKeyUp);
+            curren_form.game_timer.Tick += new EventHandler((object sender, EventArgs e) => { Move(curren_direction); });
+            move_speed = 3;
+        }
+        public void setKeysControl(Keys left, Keys up, Keys right, Keys down, Keys put) {
+            keys_control.Clear();
+            keys_now_presed.Clear();
+            keys_control.Add(left);
+            keys_now_presed.Add(left, false);
+            keys_control.Add(up);
+            keys_now_presed.Add(up, false);
+            keys_control.Add(right);
+            keys_now_presed.Add(right, false);
+            keys_control.Add(down);
+            keys_now_presed.Add(down, false);
+            keys_control.Add(put);
+            keys_now_presed.Add(put, false);
+        }
+        void ActionByKeyDown(object sender, KeyEventArgs e) {
+            if (e.KeyCode == keys_control[0]) {
+                keys_now_presed[keys_control[0]] = true;
+                curren_direction = Direction.left;
+            }
+            if (e.KeyCode == keys_control[1]) {
+                keys_now_presed[keys_control[1]] = true;
+                curren_direction = Direction.up;
+            }
+            if (e.KeyCode == keys_control[2]) {
+                keys_now_presed[keys_control[2]] = true;
+                curren_direction = Direction.right;
+            }
+            if (e.KeyCode == keys_control[3]) {
+                keys_now_presed[keys_control[3]] = true;
+                curren_direction = Direction.down;
+            }
+            if (e.KeyCode == keys_control[4]) {
+                if (putBomb != null) putBomb(x, y);
+            }
+
+
+
+        }
+        void ActionByKeyUp(object sender, KeyEventArgs e) {
+            if (e.KeyCode == keys_control[0]) {
+                keys_now_presed[keys_control[0]] = false;
+            }
+            if (e.KeyCode == keys_control[1]) {
+                keys_now_presed[keys_control[1]] = false;
+            }
+            if (e.KeyCode == keys_control[2]) {
+                keys_now_presed[keys_control[2]] = false;
+            }
+            if (e.KeyCode == keys_control[3]) {
+                keys_now_presed[keys_control[3]] = false;
+            }
+        }
+        public void Move(Direction direction) {
+
+
+
+            if (keys_now_presed[keys_control[0]] == true) {
+                if (!((curren_form.game_map.CollisionsMap[x - 1][y + COLLISIONS_COEFFICIENT] == true) || // Проверка на коллизию 1 края
+                    (curren_form.game_map.CollisionsMap[x - 1][y + m_size_y - COLLISIONS_COEFFICIENT] == true) || // Другого края
+                    (curren_form.game_map.CollisionsMap[x - 1][y + m_size_y / 2 - COLLISIONS_COEFFICIENT] == true) // Центра (когда коллизия с маленьким обьектом)
+                    ))
+                    x -= move_speed;
+            }
+            if (keys_now_presed[keys_control[1]] == true) {
+                if (!((curren_form.game_map.CollisionsMap[x + COLLISIONS_COEFFICIENT][y - 1] == true) ||
+                    (curren_form.game_map.CollisionsMap[x + m_size_x - COLLISIONS_COEFFICIENT][y - 1] == true) ||
+                    (curren_form.game_map.CollisionsMap[x + m_size_x / 2 - COLLISIONS_COEFFICIENT][y - 1] == true)))
+                    y -= move_speed;
+
+            }
+            if (keys_now_presed[keys_control[2]] == true) {
+                if (!((curren_form.game_map.CollisionsMap[x + m_size_x][y + COLLISIONS_COEFFICIENT] == true) ||
+                    (curren_form.game_map.CollisionsMap[x + m_size_x][y + m_size_y - COLLISIONS_COEFFICIENT] == true) ||
+                    (curren_form.game_map.CollisionsMap[x + m_size_x][y + m_size_y / 2 - COLLISIONS_COEFFICIENT] == true)))
+                    x += move_speed;
+            }
+            if (keys_now_presed[keys_control[3]] == true) {
+                if (!((curren_form.game_map.CollisionsMap[x + COLLISIONS_COEFFICIENT][y + m_size_y] == true) ||
+                    (curren_form.game_map.CollisionsMap[x + m_size_x - COLLISIONS_COEFFICIENT][y + m_size_y] == true) ||
+                    (curren_form.game_map.CollisionsMap[x + m_size_x / 2 - COLLISIONS_COEFFICIENT][y + m_size_y] == true)))
+                    y += move_speed;
+            }
+        }
+        public bool IsDead() {
+            return false;
+        }
+        public void Die(int die_parametr) { }
+
+    }
+    class Wall : GameObject, IIsBarrier {
+        public Wall(Form1 cf, string id, int x, int y, int x_size, int y_size) : base(cf, id, x, y, x_size, y_size) {
+            setCollision();
+        }
+
+        public void setCollision() {
+            int end_x = x + m_size_x;
+            int end_y = y + m_size_y;
+            for (int ix = x; ix < end_x; ix++)
+                for (int iy = y; iy < end_y; iy++)
+                    curren_form.game_map.CollisionsMap[ix][iy] = true;
+        }
+    }
+
+    class Bomb : GameObject, IIsBarrier {
+        public Bomb(Form1 cf, int x, int y) : base(cf, "b", x, y, 40, 40, @"..\..\GameRes\Image3.png") {
+            setCollision();
+        }
+
+        public void setCollision() {
+            const int COLLISIONS_DEPTH = 10; // TODO Обязательно сделать ромбовидную карту коллизии бомбы
+            int end_x = x + m_size_x - COLLISIONS_DEPTH;
+            int end_y = y + m_size_y - COLLISIONS_DEPTH;
+            for (int ix = x + COLLISIONS_DEPTH; ix < end_x; ix++)
+                for (int iy = y + COLLISIONS_DEPTH; iy < end_y; iy++)
+                    curren_form.game_map.CollisionsMap[ix][iy] = true;
+        }
+
+        ~Bomb() { // Тут очень важно иметь деструптор
+
+        }
+    }
+
+}
---
+++ Roughness/WindowsFormsApplication2/WindowsFormsApplication2/GameInterfaces.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApplication2 {
+    enum Direction { nope = 0, left, up, right, down }
+
+    interface IMortal {
+        bool IsDead();
+        void Die(int die_parametr);
+    }
+
+    interface IAbleToMove {
+        void Move(Direction direction);
+    }
+    interface IIsBarrier {
+        void setCollision();
+    }
+}
